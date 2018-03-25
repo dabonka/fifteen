dd6579f68fca93753a54ad9a521dce4848f62f29
Roman Belik <roman.belik@gmail.com>
2018-03-25 17:42:36 +0300

    add conditional into Cell component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,11 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Cell(props){
+	const cellPosition = props.position 
+	if (cellPosition) {
 	return(
 		<div className="cell">
-			{props.position}
+			{cellPosition}
 		</div>
 	)
+	} else {
+			return(
+		<div className="cellNull">
+			{cellPosition}
+		</div>
+	)
+
+	} 
 }
 
 Cell.propTypes = {
