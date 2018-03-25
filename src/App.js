298d7eb58369b96f1ee89f03131b22a695c5fd53
Roman Belik <roman.belik@gmail.com>
2018-03-25 21:26:50 +0300

    add class

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Header from './components/Header'
 import Cell from './components/Cell'
 import PropTypes from 'prop-types';
 
-function App(props){
-	return (
-		<div>
-			<Header title={"Пятнашки"}/>
-			<div className="mainBox">
-				{props.arr.map((n, i) => <Cell key={i} position={n}/>)}
-			</div>	
-		</div>
-	)
+class App extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<Header title={"Пятнашки"}/>
+				<div className="mainBox">
+					{this.props.arr.map((n, i) => <Cell key={i} position={n}/>)}
+				</div>	
+			</div>
+		)
+	}
+	
 }
 
 App.propTypes = {
