1d0cef7bfe7458fd4dc19316942bde76d69e7cc7
Roman Belik <roman.belik@gmail.com>
2018-04-06 21:46:29 +0300

    add functions

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Header from './components/Header'
 import Cell from './components/Cell'
 import PropTypes from 'prop-types';
 
+let currentButtonName = "Старт"
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {sequenceOfNumbers: this.props.sequenceOfNumbers};
+    this.state = {sequenceOfNumbers: this.props.sequenceOfNumbers, isGameOpen: false};
     this.shuffle = this.shuffle.bind(this);
     this.mixNumbers = this.mixNumbers.bind(this);
+    this.changeButtonName = this.changeButtonName.bind(this);
+    this.changeGameStatus = this.changeGameStatus.bind(this);
 	}
 
 	shuffle(a) {
@@ -23,6 +27,23 @@ class App extends React.Component {
 		this.setState({sequenceOfNumbers: this.shuffle(this.state.sequenceOfNumbers)});
 	}
 
+	changeGameStatus() {
+		let currentGameStatus = this.state.isGameOpen
+			if (currentGameStatus ) {
+				changeButtonName("Закрыть")
+				console.log('---', 'opened')
+			} else {
+				changeButtonName("Старт")
+				console.log('---', 'closed')
+			}
+		console.log('---', 'if false')
+		this.setState({isGameOpen: !this.isGameOpen})
+	}
+
+	changeButtonName(name){
+		let currentButtonName = name
+	}
+
 	render() {
 		return (
 			<div>
@@ -33,7 +54,7 @@ class App extends React.Component {
 
 				<div className="buttonBox">
       		<div className="buttonPosition">
-        		<button id="button" onClick={this.mixNumbers}>Перемешать</button>    
+        		<button id="button" onClick={this.changeGameStatus}>{currentButtonName}</button>    
       		</div>
     		</div>
 
@@ -44,11 +65,8 @@ class App extends React.Component {
 }
 
 App.propTypes = {
- sequenceOfNumbers: PropTypes.array.isRequired
+ sequenceOfNumbers: PropTypes.array.isRequired,
+ isGameOpen: PropTypes.bool
 }
 
 export default App;
-
-
-
-
